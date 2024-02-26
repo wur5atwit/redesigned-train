1c0a8e8b44fdc047ea870c8a01e94ef65d98f678
Richard Wu <wur5@wit.edu>
2024-02-26 16:16:43 -0500

    adds datas to crn instead of having the required columns

--- crn2Splitter.py
+++ crn2Splitter.py
@@ -1,37 +1,31 @@
 import pandas as pd
-import time
 
 class crn2Splitter:
     @staticmethod
-    def crn2Splitter(path_to_combined_crn2, output_file_path):
-        start_time = time.time()
-        df_combined_crn2 = pd.read_excel(path_to_combined_crn2)
+    def crn2Splitter(path_to_schedule, output_file_path):
+        start_time = pd.Timestamp.now()
+        df_combined_crn2 = pd.read_excel(path_to_schedule)
 
-       
         split_rows = []
 
+
         for _, row in df_combined_crn2.iterrows():
-            
+            # Split the CRN2 column into individual CRNs
             crns = row['CRN2'].split('-')
             
+            # For each CRN, create a new row with the same data as the original row but with the CRN changed
             for crn in crns:
-                # Creating a new row for each CRN
                 new_row = row.copy()
-                new_row['CRN'] = crn  # Adding the split CRN
+                new_row['CRN'] = crn  
                 split_rows.append(new_row)
 
-    
         df_split = pd.DataFrame(split_rows)
-        
-        # Rearranging columns as needed
-        column_order = ['CRN', 'CRN2', 'Count', 'SUBJECT', 'COURSE NUMBER', 'INSTRUCTOR', 'Name', 'NewTime', 'Final Exam Room', 'EXAM DAY', 'EXAM TIME']
-        df_split = df_split[column_order]  
 
-        
         df_split.to_excel(output_file_path, index=False)
 
-        end_time = time.time() 
-        execution_time = end_time - start_time 
-        print(f"Execution time to split CRN2's: {execution_time} seconds") 
+        end_time = pd.Timestamp.now()
+        print(f"Execution time to split CRN2's: {(end_time - start_time).total_seconds()} seconds")
 
         return df_split
+    
+
